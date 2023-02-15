74f7b2ab586c87ce0f5a7aa62274813ab239efae
Aakanksha Sharma <sharma.09aakanksha@gmail.com>
2023-02-15 10:53:39 +0530

    Added revising the select query 2

---
+++ HackerRank/revisiting-the-select-query-2.sql
@@ -0,0 +1,8 @@
+/*
+Problem Name: Revisting The Select Query II
+
+Problem Link: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+*/
+
+select Name from city where countrycode = 'USA' and population > 120000;
+
